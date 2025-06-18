38a6a64b4d9975e2c6830788d67b6686d3886cc4
Ashri02412323 <ashrimallick24@gmail.com>
2025-06-18 20:37:32 +0530

    Cloudinary setup and upload POST api

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,27 @@
+// src/app/api/upload/route.ts
+import { NextRequest, NextResponse } from 'next/server';
+import cloudinary from '@/lib/cloudinary';
+
+export async function POST(request: NextRequest) {
+    try {
+      const formData = await request.formData();
+      const fileBlob = formData.get('file') as Blob;
+  
+      if (!fileBlob) {
+        return NextResponse.json({ error: 'No file' }, { status: 400 });
+      }
+  
+      const buffer = await fileBlob.arrayBuffer();
+      const base64 = Buffer.from(buffer).toString('base64');
+  
+      const result = await cloudinary.uploader.upload(`data:${fileBlob.type};base64,${base64}`, {
+        folder: "nextjs-uploads",
+      });
+  
+      return NextResponse.json(result, { status: 200 });
+    } catch (err) {
+      console.error("Upload error:", err);
+      return NextResponse.json({ error: "Upload failed" }, { status: 500 });
+    }
+  }
+  
