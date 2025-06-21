3842b7fcfb5973dcd5ac286cd98592d174a7c4e2
Ashri02412323 <ashrimallick24@gmail.com>
2025-06-21 16:50:16 +0530

    Undo the latest push This reverts commit 87d921a34f6fd4e894e3b5943c7cba48d58a6acd.

--- src/components/auth/onboarding/OnboardingForm.tsx
+++ src/components/auth/onboarding/OnboardingForm.tsx
@@ -56,12 +56,12 @@ export default function OnboardingForm({ sessionImage, prefillData }: Props) {
         password: passwordFromStore,
       });
       if (signInResult?.ok) {
-        router.push('/');
+        router.push('/auth/onboarding/more-details');
       } else {
         alert('Sign in failed after registration');
       }
     } else {
-      router.push('/');
+      router.push('/auth/onboarding/more-details');
     }
   };
 
